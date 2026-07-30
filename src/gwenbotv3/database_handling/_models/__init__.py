e5977226d5173e031fbbade3738cf80809f86ea2
Zexsch <awildzexsch@gmail.com>
2026-07-30 20:59:46 +0200

    delete old handlers

--- src/gwenbotv3/database_handling/_models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Models for internal use in the database module"""
-
-from .database_connector import connect
-from .models import Server, User, UserContext
-
-__all__ = ["Server", "User", "UserContext", "connect"]
