5a61bd53d431ff1d7688ab5098346b49f89b4eba
Zexsch <awildzexsch@gmail.com>
2026-08-10 11:20:55 +0200

    Add UserPrivacy Table

---
+++ src/gwenbotv3/database/models/privacy.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from datetime import UTC, datetime
+
+from sqlalchemy import BigInteger, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from gwenbotv3.database.base import Base
+
+
+class UserPrivacy(Base):
+    __tablename__ = "user_privacy"
+    # ruff: noqa: RUF012
+    __table_args__ = {"mysql_engine": "InnoDB"}
+
+    user_id: Mapped[int] = mapped_column(BigInteger, nullable=False, primary_key=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=lambda: datetime.now(UTC),
+        nullable=False,
+    )
