cfc5950019338fc5a6969fd72fb495dc30375995
Zexsch <awildzexsch@gmail.com>
2026-08-13 17:53:52 +0200

    Script for alembic revisions

---
+++ .alembic_revision.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Pass in a revision message!"
+    exit 1
+fi
+
+CONTAINER_NAME="gwenbot-test"
+
+stopdocker() {
+  docker stop "$CONTAINER_NAME" &>/dev/null
+  exit 0
+}
+
+trap stopdocker SIGINT
+
+docker run -d --rm -v "$CONTAINER_NAME"-data:/var/lib/mysql --name "$CONTAINER_NAME" -p 3307:3306 -e MYSQL_ROOT_PASSWORD=test -e MYSQL_DATABASE=gwenbot mysql:8
+
+count=0
+sleep_count=0
+until [ "$count" -ge 2 ]; do
+  echo "Waiting for MySQL to initialise. Time passed: $sleep_count s"
+  count=$(docker logs "$CONTAINER_NAME" 2>&1 | grep -c "ready for connections")
+  sleep 1
+  sleep_count=$(($sleep_count+1))
+done
+
+ENV_FILE=.env.dev uv run alembic revision --autogenerate "$1"
+ENV_FILE=.env.dev uv run alembic upgrade head 
+
+docker stop "$CONTAINER_NAME" &>/dev/null
+exit 0
