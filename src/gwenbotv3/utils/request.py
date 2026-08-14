6bf3096c7d6e2fd79e94045638615101b25ea377
Zexsch <awildzexsch@gmail.com>
2026-08-14 22:35:52 +0200

    Try to fix ugg block x2

--- src/gwenbotv3/utils/request.py
+++ src/gwenbotv3/utils/request.py
@@ -5,6 +5,7 @@ instead of using requests.get directly.
 """
 
 import logging
+import random
 import time
 
 import requests
@@ -36,15 +37,25 @@ def request(url: str, headers: dict[str, str] | None = None) -> requests.Respons
     logger = logging.getLogger(__name__)
     logger.debug("Requesting url %s with headers %s", url, headers)
 
+    session = requests.Session()
+    session.headers.update(headers)
+
     try:
-        response = requests.get(url=url, headers=headers, timeout=10)
+        response = session.get(url=url, headers=headers, timeout=10)
     except requests.exceptions.Timeout as exc:
         raise FailedRequestError(url=url, headers=headers, reason="Timeout") from exc
+
+    if response.status_code == 403:
+        for _ in range(3):
+            response = session.get(url=url, headers=headers, timeout=10)
+            if response.ok:
+                break
+            time.sleep(random.uniform(2, 5))
 
     while response.status_code == 429:
         time.sleep(int(response.headers.get("Retry-After", 1)))
         try:
-            response = requests.get(url=url, headers=headers, timeout=10)
+            response = session.get(url=url, headers=headers, timeout=10)
         except requests.exceptions.Timeout as exc:
             raise FailedRequestError(
                 url=url, headers=headers, reason="Timeout"
